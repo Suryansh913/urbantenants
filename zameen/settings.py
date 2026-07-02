49fa29b487687bb6ee18498b71d24f273f6828ea
Suryansh Shukla <suryanshshukla82@gmail.com>
2026-07-02 08:01:17 +0530

    first commit

--- zameen/settings.py
+++ zameen/settings.py
@@ -85,20 +85,19 @@ from django.conf import settings
 # ── DATABASE ───────────────────────────────────────────
 # SQLite for everything (local + production)
 import os
+import os
 import dj_database_url
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(
-            os.environ.get("DATABASE_URL")
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+
+print("DATABASE_URL =", repr(os.environ.get("DATABASE_URL")))
+
+db = dj_database_url.parse(os.environ["DATABASE_URL"])
+print("PARSED DB =", db)
+
+DATABASES = {
+    "default": db
+}
+
+print("FINAL DATABASES =", DATABASES)
 # import dj_database_url
 # DATABASES = {
 #         "default": {
