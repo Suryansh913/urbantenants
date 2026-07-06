e339593ea7b82e42c8e49d23ed2fcbdfa1e40fcc
Suryansh Shukla <suryanshshukla82@gmail.com>
2026-07-06 12:54:17 +0530

    first commit

--- partner/admin.py
+++ partner/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Partner
 # Register your models here.
+@admin.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = (
+        "full_name",
+        "email",
+        "phone",
+        "service_type",
+        "upi_id",
+    )
+
+    search_fields = (
+        "full_name",
+        "email",
+        "phone",
+        "upi_id",
+    )
