4f2280d735ae43b11ee2e8b3f4d12d500736f3be
Suryansh Shukla <suryanshshukla82@gmail.com>
2026-09-18 17:51:46 +0530

    Add Find Roommate feature

--- partner/admin.py
+++ partner/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Partner
+from .models import Partner, Coupon, ListingPayment
 # Register your models here.
 @admin.register(Partner)
 class PartnerAdmin(admin.ModelAdmin):
@@ -18,4 +19,49 @@ class PartnerAdmin(admin.ModelAdmin):
         "upi_id",
     )
 
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "discount_percent",
+        "discount_amount",
+        "max_uses",
+        "used_count",
+        "valid_from",
+        "valid_till",
+        "active",
+    )
 
+    search_fields = ("code",)
+
+    list_filter = (
+        "active",
+        "valid_from",
+        "valid_till",
+    )
+
+
+@admin.register(ListingPayment)
+class ListingPaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "partner",
+        "cf_order_id",
+        "coupon",
+        "base_amount",
+        "discount_amount",
+        "final_amount",
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "cf_order_id",
+        "partner__full_name",
+        "partner__email",
+        "coupon__code",
+    )
+
+    list_filter = (
+        "status",
+        "created_at",
+    )
